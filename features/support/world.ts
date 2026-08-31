175bce056c4723a548cef4bb14a40292caa007af
Ryan Olson <ryolson@me.com>
2026-08-31 12:58:11 -0600

    Set up Cucumber and Playwright

---
+++ features/support/world.ts
@@ -0,0 +1,23 @@
+import { IWorldOptions, World, setWorldConstructor } from '@cucumber/cucumber';
+import type { BrowserContext, Page } from '@playwright/test';
+
+export class SidelineWorld extends World {
+  readonly baseUrl = process.env.BDD_BASE_URL ?? 'http://localhost:5173';
+  browserContext?: BrowserContext;
+  page?: Page;
+  matchingMatchesBeforeSave = 0;
+
+  constructor(options: IWorldOptions) {
+    super(options);
+  }
+
+  get currentPage(): Page {
+    if (!this.page) {
+      throw new Error('The browser page has not been initialized for this scenario.');
+    }
+
+    return this.page;
+  }
+}
+
+setWorldConstructor(SidelineWorld);
