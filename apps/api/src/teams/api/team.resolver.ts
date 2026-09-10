e7e0348d83e7002ba939abecb0a63ab6131607f3
Ryan Olson <ryolson@me.com>
2026-09-10 09:04:22 -0600

    Accept coach identity in the API

--- apps/api/src/teams/api/team.resolver.ts
+++ apps/api/src/teams/api/team.resolver.ts
@@ -1,5 +1,6 @@
-import { Args, Mutation, Query, Resolver } from '@nestjs/graphql';
+import { Args, Context, Mutation, Query, Resolver } from '@nestjs/graphql';
 import { TeamService } from '../service/team.service';
+import { optionalCoachUsernameFromCookieHeader } from './coach-username';
 import { CreateTeamInput } from './create-team.input';
 import { TeamDto } from './team.dto';
 
@@ -8,12 +9,21 @@ export class TeamResolver {
   constructor(private readonly teamService: TeamService) {}
 
   @Query(() => [TeamDto])
-  teams(): Promise<TeamDto[]> {
-    return this.teamService.findAll();
+  teams(@Context('req') request: { headers: { cookie?: string } }): Promise<TeamDto[]> {
+    const coachUsername = optionalCoachUsernameFromCookieHeader(request.headers.cookie);
+    return coachUsername
+      ? this.teamService.findAllForCoach(coachUsername)
+      : this.teamService.findAll();
   }
 
   @Mutation(() => TeamDto)
-  createTeam(@Args('input') input: CreateTeamInput): Promise<TeamDto> {
-    return this.teamService.create(input.name, input.players);
+  createTeam(
+    @Context('req') request: { headers: { cookie?: string } },
+    @Args('input') input: CreateTeamInput,
+  ): Promise<TeamDto> {
+    const coachUsername = optionalCoachUsernameFromCookieHeader(request.headers.cookie);
+    return coachUsername
+      ? this.teamService.createForCoach(coachUsername, input.name, input.players)
+      : this.teamService.create(input.name, input.players);
   }
 }
