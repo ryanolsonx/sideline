be33da1e720ade6a816db9c355fa9e128d6a71ad
Ryan Olson <ryolson@me.com>
2026-09-10 20:00:06 -0600

    Validate formation update input

--- apps/api/src/teams/api/update-team-formation.input.ts
+++ apps/api/src/teams/api/update-team-formation.input.ts
@@ -1,5 +1,6 @@
 import { Field, ID, InputType } from '@nestjs/graphql';
-import { IsUUID } from 'class-validator';
+import { Type } from 'class-transformer';
+import { IsUUID, ValidateNested } from 'class-validator';
 import { FormationInput } from './formation.input';
 
 @InputType()
@@ -9,5 +10,7 @@ export class UpdateTeamFormationInput {
   id!: string;
 
   @Field(() => FormationInput)
+  @ValidateNested()
+  @Type(() => FormationInput)
   formation!: FormationInput;
 }
